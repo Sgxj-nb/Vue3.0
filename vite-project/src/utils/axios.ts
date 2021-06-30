6ae939130e662ba9a07fcbf70248b7c3ac1551af
上官雄骏 <2059396426@qq.com>
2021-06-30 17:01:01 +0800

    asdaaa

--- vite-project/src/utils/axios.ts
+++ vite-project/src/utils/axios.ts
@@ -1,11 +1,19 @@
 import axios, { AxiosInstance } from 'axios';
-const config = require('../../config');
-const { baseURL } = config['process.env.NODE_ENV']
+// const config = require('../../config');
+// const { baseURL } = config[process.env.NODE_ENV]
 let token = localStorage.getItem('token');
+let baseURL = 'https://api.xwhx.top'
 // 创建实例
-const service: AxiosInstance = axios.create({
-  baseURL, // 接口地址
-  timeout: 200000//超时时间
+// const service: AxiosInstance = axios.create({
+//   baseURL, // 接口地址
+//   timeout: 40 * 1000//超时时间40s
+// });
+const service = axios.create({
+  baseURL,
+  timeout: 40 * 1000,
+  // headers: { // 配置OAuth 2.0的认证头
+  //   'Content-Type': 'application/json;charset=utf-8'
+  // },
 });
 
 // 请求拦截器
@@ -21,9 +29,31 @@ service.interceptors.request.use(config => {
 
 // 响应
 service.interceptors.response.use(resp => {
-  return resp;
+  return Promise.resolve(resp)
 }, error => {
   return Promise.reject(error)
-})
+});
+
+// 发起请求
+export function request(config: any) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const result = await axios({
+        ...config
+      })
+      resolve(result)
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
+// service =function  (params: object) {
+//   axios({
+//     url: service.baseURL,
+//     method: 'post',
+//     params
+//   })
+// }
 
 export default service;
