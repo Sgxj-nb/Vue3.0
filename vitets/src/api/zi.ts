600bcb3d8f2c4b4cfde84d6efc071501cf476b34
上官雄骏 <2059396426@qq.com>
2021-11-28 22:34:37 +0800

    待完善的商城代码

--- vitets/src/api/zi.ts
+++ vitets/src/api/zi.ts
@@ -1,5 +1,6 @@
 import { useCounterStore } from "@/pain/index"; // pain
 const useCounter = useCounterStore();
 export default {
-  detail: 'api/v1/goods/detail/10902'
+  detail: 'api/v1/goods/detail/10902',
+  shop: 'api/v1/shop-cart'
 }
--- vitets/src/components/phone/zi/xq.vue
+++ vitets/src/components/phone/zi/xq.vue
@@ -2,21 +2,70 @@
   <!-- 商品详情界面 -->
   <div>
     <div>
-      {{ zhinfo.duiixang }}
+      <img style="width: 100%" :src="rel.paifnoa.goodsCoverImg" alt="" />
     </div>
+  </div>
+  <div>
+    {{ rel.paifnoa.goodsName }}
+  </div>
+  <div>
+    {{ rel.paifnoa.sellingPrice }}
+  </div>
+  <div v-html="rel.paifnoa.goodsDetailContent"></div>
+  <div class="zhiinfo">购物车</div>
+  <div class="zhinfawwwd">
+    <div @click="ifnoawwddd">点击添加购物车{{ num }}</div>
   </div>
 </template>
 <script lang="ts" setup>
-import { onMounted } from "vue-demi";
+import { onMounted, reactive, ref } from "vue-demi";
 import request from "@/utils/request";
 import { useCounterStore } from "@/pain/index"; // pain
 const zhinfo: any = useCounterStore();
 
 // 请求详情界面
 function asdasdas() {
-  request.get(`goods.detail`, {}).then((res) => {
+  interface zhi {
+    message?: String;
+    data?: Object;
+  }
+  request.get(`goods.detail`, {}).then((res: zhi) => {
     console.log(res);
+    if (res.message) {
+      rel.paifnoa = res.data;
+    }
   });
 }
+let rel = reactive({
+  paifnoa: {},
+});
 asdasdas();
+
+//添加的数字
+let num = ref(0);
+
+// 点击添加
+function ifnoawwddd() {}
+
+function goouwche() {
+  interface jiesss {
+    message?: String;
+    data?: Array;
+  }
+  request.get("goods.shop", {}).then((res: jiesss) => {});
+}
 </script>
+
+<style scoped>
+.zhiinfo {
+  position: fixed;
+  top: 0;
+  color: red;
+}
+.zhinfawwwd {
+  color: red;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+</style>
