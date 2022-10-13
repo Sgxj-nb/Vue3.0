026e82deaf09878750e446210ee74e8bdde14e74
上官雄骏 <w-shangguanxiongjun@rootcloud.com>
2022-10-13 23:57:53 +0800

    完成修改密码组件初步搭建

--- main/src/components/Herder.vue
+++ main/src/components/Herder.vue
@@ -12,25 +12,65 @@
       <div style="flex: 1; visibility: hidden">s</div>
       <div class="S-right">
         <span><img :src="img" alt="" /></span>
-        <span>管理员</span>
-        <span>
-          <down-outlined />
-        </span>
+        <a-dropdown>
+          <a @click.prevent
+            >管理员
+            <DownOutlined />
+          </a>
+          <template #overlay>
+            <a-menu @click="onMeun">
+              <a-menu-item :key="1"> 修改密码 </a-menu-item>
+              <a-menu-item :key="2"> 推出登录 </a-menu-item>
+              <a-menu-item :key="3"> 消息 </a-menu-item>
+            </a-menu>
+          </template>
+        </a-dropdown>
       </div>
     </div>
   </div>
+  <!-- 修改密码组件 -->
+  <Modal :visible="visible" @onClose="onClose"></Modal>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import RedoOutlined from "@ant-design/icons-vue/RedoOutlined";
 import DoubleLeftOutlined from "@ant-design/icons-vue/DoubleLeftOutlined";
 import SearchOutlined from "@ant-design/icons-vue/SearchOutlined";
 import FullscreenOutlined from "@ant-design/icons-vue/FullscreenOutlined";
 import AppleOutlined from "@ant-design/icons-vue/AppleOutlined";
 import DownOutlined from "@ant-design/icons-vue/DownOutlined";
+import Modal from "./herdercom/index.vue";
+import { Dropdown, Menu, MenuItem } from "ant-design-vue";
 import { title } from "@/tools/tools";
 let img: string = "https://qmplusimg.henrongyi.top/gva_header.jpg";
 let titleName = title;
+let visible = ref<boolean>(false);
+
+interface meun {
+  domEvent: Object;
+  eventKey: String;
+  eventKeyPath: Array<String>;
+  item: Object;
+  key: Number;
+  keyPath: Array<String>;
+}
+
+// 下拉框点击事件
+function onMeun(e: meun) {
+  console.log(e.key);
+  const key = e.key;
+  if (key == 1) {
+    visible.value = true;
+  } else if (key == 2) {
+  } else {
+  }
+}
+
+// 关闭弹出层(密码)
+function onClose() {
+  visible.value = false;
+}
 </script>
 
 <style scoped>
@@ -44,11 +84,11 @@ let titleName = title;
   cursor: pointer;
 }
 .S-right > span {
-  display: inline-block;
+  /* display: inline-block; */
 }
-.S-right > span:nth-child(4) {
+/* .S-right > span:nth-child(4) {
   width: 50px;
-}
+} */
 .S-right > span:nth-child(5) {
   font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB,
     Microsoft YaHei, \5fae\8f6f\96c5\9ed1, Arial, sans-serif;
---
+++ main/src/components/herdercom/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <a-modal
+      v-model:visible="visible"
+      title="修改密码"
+      @ok="onSuer"
+      okText="确认"
+      cancelText="取消"
+    >
+      <div>我是修改面</div>
+    </a-modal>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive } from "vue";
+import { Modal } from "ant-design-vue";
+
+const props = defineProps({
+  // 弹出层控制参数
+  visible: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const emit = defineEmits(["onClose"]);
+
+// 调用父组件方法改变visible的值
+function onChange() {
+  emit("onClose");
+}
+
+function onSuer() {
+  emit("onClose");
+}
+</script>
+
+<style scoped></style>
